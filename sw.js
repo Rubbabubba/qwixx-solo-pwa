f6c7369329aa617aefad14497ee885a82b73119e
Rubbabubba <47430510+Rubbabubba@users.noreply.github.com>
2026-01-09 21:47:52 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "qwixx-solo-v1";
+const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : Promise.resolve())))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
